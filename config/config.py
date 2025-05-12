47ea887a6bd6e80dd0da42086143d1ff919ceb2d
Yosri1 <zayaniyosri10@gmail.com>
2025-05-12 09:22:19 +0100

    new repo

---
+++ config/config.py
@@ -0,0 +1,3 @@
+# config/config.py
+DATABASE_URL = "postgresql://JobPostingsBusiness_owner:npg_F0WKEspe4hzw@ep-restless-meadow-a4ku5fkh-pooler.us-east-1.aws.neon.tech/JobPostingsBusiness?sslmode=require"
+GOOGLE_API_KEY = "AIzaSyAk3lNuzCUkRFiuPlWx8S8jmWDVo_Hn2TI"
---
+++ utils/db_utils.py
@@ -0,0 +1,48 @@
+# utils/db_utils.py
+from sqlalchemy import create_engine
+import pandas as pd
+import uuid
+import logging
+from datetime import datetime
+logger = logging.getLogger(__name__)
+
+def get_engine(database_url):
+    """Create and return a SQLAlchemy engine."""
+    return create_engine(database_url)
+
+def save_to_db_non_dupe(job_data, engine):
+    """Save job postings to PostgreSQL database."""
+    if not job_data:
+        logger.info("No new job postings to save.")
+        return
+    
+    df = pd.DataFrame(job_data)
+    df['ID'] = [str(uuid.uuid4()) for _ in range(len(df))]
+    df['Scraped'] = datetime.now().date()
+
+    columns = ["ID","Major","JobTitle", "Published", "JobType", "WorkLocation", "Experience", 
+               "Education", "Availability", "Langues", "Entreprise", 
+               "Sector", "Size", "Description", "Source","Scraped"]
+    df = df.reindex(columns=columns)
+    
+    with engine.connect() as conn:
+        df.to_sql('job_postings', conn, if_exists='replace', index=False)
+    logger.info(f"Successfully saved {len(df)} non duped job postings to database.")
+def save_to_db(job_data, engine):
+    """Save job postings to PostgreSQL database."""
+    if not job_data:
+        logger.info("No new job postings to save.")
+        return
+    
+    df = pd.DataFrame(job_data)
+    df['ID'] = [str(uuid.uuid4()) for _ in range(len(df))]
+    df['Scraped'] = datetime.now().date()
+
+    columns = ["ID","Major","JobTitle", "Published", "JobType", "WorkLocation", "Experience", 
+               "Education", "Availability", "Langues", "Entreprise", 
+               "Sector", "Size", "Description", "Source","Scraped",]
+    df = df.reindex(columns=columns)
+    
+    with engine.connect() as conn:
+        df.to_sql('job_postings', conn, if_exists='append', index=False)
+    logger.info(f"Successfully saved {len(df)} new job postings to database.")
